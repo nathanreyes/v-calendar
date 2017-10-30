06e0e9786f990bfe132b1b95f4a7561f32c42cb7
Nathan Reyes <nathanreyes@me.com>
2017-10-30 05:30:35 -0500

    Fix attribute API reference.

--- docs/components/api/CalendarApiAttribute.vue
+++ docs/components/api/CalendarApiAttribute.vue
@@ -41,12 +41,12 @@ export default {
       },
       {
         propertyName: '<code>contentStyle: Object</code>',
-        description: 'Day cell content style to associate with the attribute. This property is inherited from the default <code>dayContentStyle</code> calendar property, so only assign this property if it needs to be overwritten by the attribute.',
+        description: 'Day cell content style to associate with the attribute.',
         default: '<code>undefined</code>',
       },
       {
         propertyName: '<code>contentHoverStyle: Object</code>',
-        description: 'This style is merged with the <code>contentStyle</code> property when the content is hovered. This property is inherited from the default <code>dayContentHoverStyle</code> calendar property, so only assign this property if it needs to be overwritten by the attribute.',
+        description: 'This style is merged with the <code>contentStyle</code> property when the content is hovered.',
         default: '<code>undefined</code>',
       },
       {
