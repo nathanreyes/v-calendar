28b05a39bb095830086ff7bc51572a650e4cfab2
Nathan Reyes <nathanreyes@me.com>
2017-12-29 08:54:37 -0600

    Add babel-polyfill for docs.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -21,7 +21,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    docs: './docs/docs.js',
+    docs: ['babel-polyfill', './docs/docs.js'],
     ['v-calendar']: './src/lib.js'
   },
   output: {
