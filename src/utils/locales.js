52736eeb22aced0e318a1dc67a8ec06dfa047a90
kadoshms <mor@decor-d.com>
2018-08-21 16:09:51 +0300

    added "HE" (Hebrew) locale

--- src/utils/locales.js
+++ src/utils/locales.js
@@ -74,6 +74,8 @@ const locales = {
   tr: { dow: 2, L: 'DD.MM.YYYY' },
   // Ukrainian
   uk: { dow: 2, L: 'DD.MM.YYYY' },
+  // Hebrew
+  he: { dow: 1, L: 'DD.MM.YYYY' },
 };
 locales.en = locales['en-US'];
 locales.zh = locales['zh-CN'];
