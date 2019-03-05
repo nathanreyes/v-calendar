2b8bbc0eeda58cc3774f1749d10cb97f4a7b7e10
Nathan Reyes <nathanreyes@me.com>
2019-03-05 16:47:09 -0600

    Modify 'single' test component

--- docs/.vuepress/components/tests/single.vue
+++ docs/.vuepress/components/tests/single.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="example">
-    <v-date-picker v-model="date" is-inline/>
+    <div class="flex mb-2">
+      <div>
+        <input type="radio" id="single" value="single" v-model="mode">
+        <label for="single">Single</label>
+      </div>
+      <div class="ml-2">
+        <input type="radio" id="multiple" value="multiple" v-model="mode">
+        <label for="multiple">Multiple</label>
+      </div>
+      <div class="ml-2">
+        <input type="radio" id="range" value="range" v-model="mode">
+        <label for="range">Range</label>
+      </div>
+    </div>
+    <div class="flex items-center mb-2">
+      <input id="isInline" type="checkbox" v-model="isInline" class="mt-1 mr-1">
+      <label for="isInline">Is Inline</label>
+    </div>
+    <v-date-picker v-model="date" :mode="mode" :is-inline="isInline"/>
   </div>
 </template>
 
@@ -8,18 +26,11 @@
 export default {
   data() {
     return {
-      date: new Date(),
+      date: null,
+      mode: 'single',
+      isInline: true,
       popoverVisibility: 'visible',
     };
   },
-  // computed: {
-  //   selectAttribute() {
-  //     return {
-  //       popover: {
-  //         visibility: this.popoverVisibility,
-  //       },
-  //     };
-  //   },
-  // },
 };
 </script>
