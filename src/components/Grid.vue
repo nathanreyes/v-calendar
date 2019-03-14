7367a851bd2136d1fd582f9cc523113ae6ac9e45
Nathan Reyes <nathanreyes@me.com>
2019-03-14 09:49:17 -0500

    Update Grid.vue

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -23,12 +23,22 @@ export default {
       const cells = [];
       for (let r = 1, p = 1; r <= this.rows; r++) {
         for (let c = 1; c <= this.columns; c++) {
-          // Actually add the cell components
+          // Add the cell components
           cells.push(
             h(
               'div',
               {
-                class: 'c-grid-cell',
+                class: [
+                  'vc-grid-cell',
+                  `vc-grid-cell-row-${r}`,
+                  `vc-grid-cell-col-${c}`,
+                  {
+                    'vc-grid-cell-row-first': r === 1,
+                    'vc-grid-cell-row-last': r === this.rows,
+                    'vc-grid-cell-col-first': c === 1,
+                    'vc-grid-cell-col-last': c === this.columns,
+                  },
+                ],
                 style: {
                   'grid-row': r,
                   'grid-column': c,
@@ -62,8 +72,6 @@ export default {
       default: 1,
     },
     autofit: Boolean,
-    gap: String,
-    innerBorderClass: String,
     columnWidth: {
       type: String,
       default: '1fr',
