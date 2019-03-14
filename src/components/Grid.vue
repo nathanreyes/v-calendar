a82969b2d1993b1b30fdaf12208b357908399c89
Nathan Reyes <nathanreyes@me.com>
2019-03-14 09:12:41 -0500

    Get basic layout working.

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,43 +1,6 @@
 <script>
 export default {
   render(h) {
-    // Grid divider renderer
-    const getDivider = ({ isHorizontal = true, row, column, span }) =>
-      h(
-        'div',
-        {
-          class: [
-            'vc-grid-divider-container',
-            isHorizontal ? 'is-horizontal' : 'is-vertical',
-          ],
-          style: {
-            ...(isHorizontal && {
-              'grid-row': row,
-              'grid-column': `1 / span ${span}`,
-              height: this.gap,
-            }),
-            ...(!isHorizontal && {
-              'grid-column': column,
-              'grid-row': `1 / span ${span}`,
-              width: this.gap,
-            }),
-          },
-        },
-        [
-          h('div', {
-            class: [
-              this.innerBorderClass,
-              isHorizontal ? 'is-horizontal' : 'is-vertical',
-            ],
-            style: {
-              borderBottomWidth: '0',
-              borderRightWidth: '0',
-              flexGrow: '1',
-            },
-          }),
-        ],
-      );
-
     // Grid cell renderer
     const getCell = ({ position, row, column }) => {
       // Get the default slot first
@@ -58,52 +21,22 @@ export default {
     // Grid cells renderer
     const getCells = () => {
       const cells = [];
-      const totalRows = this.rows * 2 - 1;
-      const totalColumns = this.columns * 2 - 1;
-      for (let r = 1, p = 1; r <= totalRows; r++) {
-        for (let c = 1; c <= totalColumns; c++) {
-          // Add horizontal divider for non-zero even rows in the 1st column
-          if (r % 2 === 0) {
-            if (c === 1) {
-              cells.push(
-                getDivider({
-                  isHorizontal: true,
-                  row: r,
-                  column: c,
-                  span: totalColumns,
-                }),
-              );
-            }
-          } else {
-            // Add vertical dividers for non-zero even columns in the 1st row
-            if (c % 2 === 0) {
-              if (r === 1) {
-                cells.push(
-                  getDivider({
-                    isHorizontal: false,
-                    row: r,
-                    column: c,
-                    span: totalRows,
-                  }),
-                );
-              }
-            } else {
-              // Actually add the cell components
-              cells.push(
-                h(
-                  'div',
-                  {
-                    class: 'c-grid-cell',
-                    style: {
-                      'grid-row': r,
-                      'grid-column': c,
-                    },
-                  },
-                  [getCell({ position: p++, row: r, column: c })],
-                ),
-              );
-            }
-          }
+      for (let r = 1, p = 1; r <= this.rows; r++) {
+        for (let c = 1; c <= this.columns; c++) {
+          // Actually add the cell components
+          cells.push(
+            h(
+              'div',
+              {
+                class: 'c-grid-cell',
+                style: {
+                  'grid-row': r,
+                  'grid-column': c,
+                },
+              },
+              [getCell({ position: p++, row: r, column: c })],
+            ),
+          );
         }
       }
       return cells;
@@ -145,7 +78,7 @@ export default {
     gridTemplateColumns() {
       return `repeat(${this.autofit ? 'auto-fit' : this.columns}, ${
         this.columnWidth
-      } auto)`;
+      })`;
     },
   },
 };
@@ -156,14 +89,5 @@ export default {
   position: relative;
   flex-shrink: 1;
   display: grid;
-}
-
-.vc-grid-divider-container {
-  display: flex;
-  align-items: center;
-}
-
-.vc-grid-divider-container.is-vertical {
-  flex-direction: column;
 }
 </style>
