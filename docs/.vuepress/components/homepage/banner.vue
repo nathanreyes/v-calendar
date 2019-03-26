7bc0d6e2e8ed1d490fb24178537715e836044acc
Nathan Reyes <nathanreyes@me.com>
2019-03-26 17:10:55 -0500

    Docs changes

--- docs/.vuepress/components/homepage/banner.vue
+++ docs/.vuepress/components/homepage/banner.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-600 text-white px-2 py-3 text-center">
-    <p class="text-lg font-bold mt-0 mb-0">This documentation applies for v1.0.0-beta.1</p>
-    <p class="text-gray-200 my-0">
+    <p class="text font-bold mt-0 mb-0">This documentation applies for v1.0.0-beta.1</p>
+    <p class="text-sm text-gray-200 my-0">
       <a href="#" class="underline text-gray-200">Click here</a> to reference documentation for previous versions
     </p>
   </div>
