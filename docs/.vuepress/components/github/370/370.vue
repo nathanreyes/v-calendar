9111a443396cb8b138a2bbe3eadad6fe8006fdf7
Nathan Reyes <nathanreyes@me.com>
2019-07-14 15:34:18 -0500

    Add Github issue 370

---
+++ docs/.vuepress/components/github/370/370.vue
@@ -0,0 +1,33 @@
+	<template>
+  <div>
+    <button @click="toggleLocale">Toggle locale</button>
+    <v-date-picker
+      ref="calendar"
+      v-model="range"
+      mode="range"
+      is-inline
+      :locale="locale"
+    />
+  </div>
+</template>
+
+  <script>
+export default {
+  data() {
+    return {
+      locale: {
+        id: 'de',
+        firstDayOfWeek: 2,
+        masks: { weekdays: 'WW' },
+      },
+      range: {},
+    };
+  },
+  methods: {
+    toggleLocale() {
+      this.locale.id = this.locale.id === 'fr' ? 'de' : 'fr';
+    },
+  },
+};
+</script>
+  
