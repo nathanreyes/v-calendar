ddf4a5bd314f16e06408c6548584acc139c10cd0
Nathan Reyes <nathanreyes@me.com>
2019-08-06 16:22:35 -0500

    Add github issue for disable day popovers

---
+++ docs/.vuepress/components/github/379/379.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-date-picker
+    v-model="value"
+    mode="range"
+    :attributes="attrs"
+    :disabled-dates="disabledDates"
+    is-inline
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      attrs: [
+        {
+          dates: new Date(),
+          dot: true,
+          popover: {
+            label: 'This is a test',
+          },
+        },
+      ],
+      disabledDates: [new Date()],
+      value: null,
+    };
+  },
+};
+</script>
