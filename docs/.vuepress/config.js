7ad25d544d1d5799b6aa326234530f8f0a484a25
Nathan Reyes <nathanreyes@me.com>
2019-09-19 08:13:54 -0500

    Remove theme styles section from API

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,7 +66,6 @@ module.exports = {
             'day-object',
             'attribute',
             'datepicker',
-            'theme-styles',
             'date-patterns',
           ],
         },
