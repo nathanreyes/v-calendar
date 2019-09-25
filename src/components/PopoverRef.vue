368723d68e49b304ecf7e6b9d21da6b5700aa7cb
Nathan Reyes <nathanreyes@me.com>
2019-09-25 08:39:30 -0500

    Fix `PopoverRef` bug with `focus` visibility

--- src/components/PopoverRef.vue
+++ src/components/PopoverRef.vue
@@ -107,11 +107,7 @@ export default {
       }
     },
     onFocusOut(e) {
-      if (
-        this.isFocused &&
-        e.relatedTarget &&
-        !elementContains(this.reference, e.relatedTarget)
-      ) {
+      if (this.isFocused && !elementContains(this.reference, e.relatedTarget)) {
         this.isFocused = false;
         if (this.visibility.includes('focus')) {
           this.refreshVisibility();
