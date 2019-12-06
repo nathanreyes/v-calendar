b7229bdc92a65b649ae743ced2628a9791288d3f
Nathan Reyes <nathanreyes@me.com>
2019-12-06 16:50:35 -0600

    Formatting

--- docs/.vuepress/theme/components/NavLink.vue
+++ docs/.vuepress/theme/components/NavLink.vue
@@ -5,7 +5,8 @@
     @focusout.native="focusoutAction"
     v-if="!isExternal(link)"
     :exact="exact"
-  >{{ item.text }}</router-link>
+    >{{ item.text }}</router-link
+  >
   <a
     v-else
     :href="link"
@@ -15,40 +16,42 @@
     :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'"
   >
     {{ item.text }}
-    <OutboundLink/>
+    <OutboundLink />
   </a>
 </template>
 
 <script>
-import { isExternal, isMailto, isTel, ensureExt } from '../util'
+import { isExternal, isMailto, isTel, ensureExt } from '../util';
 
 export default {
   props: {
     item: {
-      required: true
-    }
+      required: true,
+    },
   },
 
   computed: {
-    link () {
-      return ensureExt(this.item.link)
+    link() {
+      return ensureExt(this.item.link);
     },
 
-    exact () {
+    exact() {
       if (this.$site.locales) {
-        return Object.keys(this.$site.locales).some(rootLink => rootLink === this.link)
+        return Object.keys(this.$site.locales).some(
+          rootLink => rootLink === this.link,
+        );
       }
-      return this.link === '/'
-    }
+      return this.link === '/';
+    },
   },
 
   methods: {
     isExternal,
     isMailto,
     isTel,
-    focusoutAction () {
-      this.$emit('focusout')
-    }
-  }
-}
+    focusoutAction() {
+      this.$emit('focusout');
+    },
+  },
+};
 </script>
--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -5,144 +5,118 @@
     @touchstart="onTouchStart"
     @touchend="onTouchEnd"
   >
-    <Navbar
-      v-if="shouldShowNavbar"
-      @toggle-sidebar="toggleSidebar"
-    />
+    <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar" />
 
-    <div
-      class="sidebar-mask"
-      @click="toggleSidebar(false)"
-    ></div>
+    <div class="sidebar-mask" @click="toggleSidebar(false)"></div>
 
-    <Sidebar
-      :items="sidebarItems"
-      @toggle-sidebar="toggleSidebar"
-    >
-      <slot
-        name="sidebar-top"
-        #top
-      />
-      <slot
-        name="sidebar-bottom"
-        #bottom
-      />
+    <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar">
+      <slot name="sidebar-top" #top />
+      <slot name="sidebar-bottom" #bottom />
     </Sidebar>
 
-    <Home v-if="$page.frontmatter.home"/>
+    <Home v-if="$page.frontmatter.home" />
 
-    <Page
-      v-else
-      :sidebar-items="sidebarItems"
-    >
-      <slot
-        name="page-top"
-        #top
-      />
-      <slot
-        name="page-bottom"
-        #bottom
-      />
+    <Page v-else :sidebar-items="sidebarItems">
+      <slot name="page-top" #top />
+      <slot name="page-bottom" #bottom />
     </Page>
   </div>
 </template>
 
 <script>
-import Home from '@theme/components/Home.vue'
-import Navbar from '@theme/components/Navbar.vue'
-import Page from '@theme/components/Page.vue'
-import Sidebar from '@theme/components/Sidebar.vue'
-import { resolveSidebarItems } from '../util'
+import Home from '@theme/components/Home.vue';
+import Navbar from '@theme/components/Navbar.vue';
+import Page from '@theme/components/Page.vue';
+import Sidebar from '@theme/components/Sidebar.vue';
+import { resolveSidebarItems } from '../util';
 
 export default {
   components: { Home, Page, Sidebar, Navbar },
 
-  data () {
+  data() {
     return {
-      isSidebarOpen: false
-    }
+      isSidebarOpen: false,
+    };
   },
 
   computed: {
-    shouldShowNavbar () {
-      const { themeConfig } = this.$site
-      const { frontmatter } = this.$page
-      if (
-        frontmatter.navbar === false
-        || themeConfig.navbar === false) {
-        return false
+    shouldShowNavbar() {
+      const { themeConfig } = this.$site;
+      const { frontmatter } = this.$page;
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
+        return false;
       }
       return (
-        this.$title
-        || themeConfig.logo
-        || themeConfig.repo
-        || themeConfig.nav
-        || this.$themeLocaleConfig.nav
-      )
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
+      );
     },
 
-    shouldShowSidebar () {
-      const { frontmatter } = this.$page
+    shouldShowSidebar() {
+      const { frontmatter } = this.$page;
       return (
-        !frontmatter.home
-        && frontmatter.sidebar !== false
-        && this.sidebarItems.length
-      )
+        !frontmatter.home &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
+      );
     },
 
-    sidebarItems () {
+    sidebarItems() {
       return resolveSidebarItems(
         this.$page,
         this.$page.regularPath,
         this.$site,
-        this.$localePath
-      )
+        this.$localePath,
+      );
     },
 
-    pageClasses () {
-      const userPageClass = this.$page.frontmatter.pageClass
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass;
       return [
         {
           'no-navbar': !this.shouldShowNavbar,
           'sidebar-open': this.isSidebarOpen,
-          'no-sidebar': !this.shouldShowSidebar
+          'no-sidebar': !this.shouldShowSidebar,
         },
-        userPageClass
-      ]
-    }
+        userPageClass,
+      ];
+    },
   },
 
-  mounted () {
+  mounted() {
     this.$router.afterEach(() => {
-      this.isSidebarOpen = false
-    })
+      this.isSidebarOpen = false;
+    });
   },
 
   methods: {
-    toggleSidebar (to) {
-      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-      this.$emit('toggle-sidebar', this.isSidebarOpen)
+    toggleSidebar(to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen;
+      this.$emit('toggle-sidebar', this.isSidebarOpen);
     },
 
     // side swipe
-    onTouchStart (e) {
+    onTouchStart(e) {
       this.touchStart = {
         x: e.changedTouches[0].clientX,
-        y: e.changedTouches[0].clientY
-      }
+        y: e.changedTouches[0].clientY,
+      };
     },
 
-    onTouchEnd (e) {
-      const dx = e.changedTouches[0].clientX - this.touchStart.x
-      const dy = e.changedTouches[0].clientY - this.touchStart.y
+    onTouchEnd(e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x;
+      const dy = e.changedTouches[0].clientY - this.touchStart.y;
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
         if (dx > 0 && this.touchStart.x <= 80) {
-          this.toggleSidebar(true)
+          this.toggleSidebar(true);
         } else {
-          this.toggleSidebar(false)
+          this.toggleSidebar(false);
         }
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
