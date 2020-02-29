b97cbee1850ff53b2a4726736fb3c52e3861a25c
Nathan Reyes <nathanreyes@me.com>
2020-02-29 12:23:29 -0600

    Add github issue 272

---
+++ docs/.vuepress/components/github/272.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-date-picker
+    class="w-full"
+    mode="range"
+    v-model="eventDates"
+    :masks="masks"
+  ></v-date-picker>
+</template>
+
+<script>
+export default {
+  githubTitle: `Typed dates are not being parsed correctly`,
+  data() {
+    return {
+      eventDates: null,
+      masks: {
+        input: ['YYYY-MM-DD'],
+        data: ['YYYY-MM-DD'],
+      },
+    };
+  },
+};
+</script>
