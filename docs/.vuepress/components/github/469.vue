b49195b1c516af6e3933d63f2c622bf6d0772907
Nathan Reyes <nathanreyes@me.com>
2020-04-15 21:36:20 -0500

    Add github issues

---
+++ docs/.vuepress/components/github/469.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-calendar :locale="calendarEnv" />
+</template>
+
+<script>
+export default {
+  githubTitle: 'How to use the monthNames in locale?',
+  data() {
+    return {
+      calendarEnv: {
+        masks: {
+          title: 'YYYY-MM',
+        },
+        monthNames: [
+          'A',
+          'B',
+          'C',
+          'D',
+          'E',
+          'F',
+          'G',
+          'H',
+          'I',
+          'J',
+          'K',
+          'L',
+        ],
+      },
+    };
+  },
+};
+</script>
