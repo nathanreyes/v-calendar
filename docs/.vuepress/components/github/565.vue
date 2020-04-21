009b7537a4928b31262e793958cbe3e7d80a5a96
Nathan Reyes <nathanreyes@me.com>
2020-04-21 16:16:30 -0500

    Add github example

---
+++ docs/.vuepress/components/github/565.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-date-picker :mode="mode" v-model="selectedDate" locale="es-MX" />
+  </div>
+</template>
+
+<script>
+// export const title = 'v-date-picker is-dark turns the field dark as well';
+
+export default {
+  githubTitle: 'Error: Invalid Date in fecha.format',
+  data() {
+    return {
+      mode: 'single',
+      selectedDate: null,
+    };
+  },
+};
+</script>
