03019d85f7ae57ce944156513bcaa8f21a0c1f11
Nathan Reyes <nathanreyes@me.com>
2020-07-18 11:14:41 -0500

    Homepage datpicker example

--- docs/.vuepress/components/homepage/datepicker.vue
+++ docs/.vuepress/components/homepage/datepicker.vue
@@ -11,17 +11,28 @@
     </div>
     <p class="text-lg font-medium text-gray-600">Popover Mode</p>
     <div class="example-home mt-0">
-      <div class="w-full max-w-sm">
+      <div class="w-full max-w-md">
         <form class="bg-white shadow-md rounded px-8 pt-6 pb-8" @submit.prevent>
           <div class="mb-4">
             <span class="block text-gray-700 text-sm text-left font-bold mb-2"
               >Select Range</span
             >
             <v-date-picker mode="range" v-model="range2">
-              <template v-slot="{ inputValue, inputEvents }">
-                <div class="flex w-full">
-                  <input :value="inputValue.start" v-bind="inputEvents" />
-                  <input :value="inputValue.start" v-bind="inputEvents" />
+              <template v-slot="{ inputValue, inputEvents, isDragging }">
+                <div class="flex justify-start items-center">
+                  <input
+                    class="flex-grow px-2 py-1 bg-gray-100 border rounded"
+                    :class="isDragging ? 'text-gray-600' : 'text-gray-900'"
+                    :value="inputValue.start"
+                    v-on="inputEvents.start"
+                  />
+                  <span class="flex-shrink-0 mx-2">&#8594;</span>
+                  <input
+                    class="flex-grow px-2 py-1 bg-gray-100 border rounded"
+                    :class="isDragging ? 'text-gray-600' : 'text-gray-900'"
+                    :value="inputValue.end"
+                    v-on="inputEvents.end"
+                  />
                 </div>
               </template>
             </v-date-picker>
