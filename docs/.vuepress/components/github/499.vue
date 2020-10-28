66ad04c6a410edd9013f76ce88c38b001b1858d1
Nathan Reyes <nathanreyes@me.com>
2020-10-27 21:43:31 -0500

    Add example 499

---
+++ docs/.vuepress/components/github/499.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-calendar :attributes="attrs" />
+</template>
+
+<script>
+export default {
+  githubTitle: `contentStyle not working since 1.0.x`,
+  data() {
+    return {
+      attrs: [
+        {
+          // highlight: {
+          //   contentStyle: {
+          //     fontWeight: '700',
+          //     fontSize: '.9rem',
+          //     color: 'red',
+          //   },
+          // },
+          content: {
+            style: {
+              fontWeight: '700',
+              fontSize: '.9rem',
+              color: 'red',
+            },
+          },
+          dates: { start: new Date(), end: new Date(2020, 9, 30) },
+          popover: {
+            label: 'Today',
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
