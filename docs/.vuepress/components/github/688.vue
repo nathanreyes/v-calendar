f11b172958eef4383a12b4ef066fad5265542d42
Nathan Reyes <nathanreyes@me.com>
2020-11-01 14:37:06 -0600

    Create 688.vue

---
+++ docs/.vuepress/components/github/688.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-calendar trim-weeks :rows="1" />
+    <!-- <v-date-picker v-model="date" trim-weeks>
+      <template #default="{ inputValue, inputEvents }">
+        <input :value="inputValue" v-on="inputEvents" />
+      </template>
+    </v-date-picker> -->
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: `Trim weeks`,
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+};
+</script>
