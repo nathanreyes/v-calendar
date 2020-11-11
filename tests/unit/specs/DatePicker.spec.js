50b6047e169801e685341a8645e9699221d80fb3
Nathan Reyes <nathanreyes@me.com>
2020-11-10 22:21:16 -0600

    Update component tests

--- tests/unit/specs/DatePicker.spec.js
+++ tests/unit/specs/DatePicker.spec.js
@@ -1,17 +1,20 @@
-// import { mount } from '@vue/test-utils';
-// import DatePicker from '@/components/DatePicker';
+import { mount } from '@vue/test-utils';
+import DatePicker from '@/components/DatePicker';
 
 describe('DatePicker', () => {
-  it('renders props.fromPage', () => {
-    // const fromPage = { month: 4, year: 2001 };
-    // const wrapper = mount(DatePicker, {
-    //   propsData: {
-    //     fromPage,
-    //     isInline: true,
-    //   },
-    // });
-    // const cal = wrapper.find('div');
-    // console.log(cal.html());
-    // expect(2).toBe(2);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(DatePicker, {
+      propsData: {
+        value: new Date(2000, 0, 1),
+      },
+    });
+  });
+
+  describe(':props', () => {
+    it(':value - renders a date value', async () => {
+      expect(wrapper.find('.id-2000-01-01').exists()).toBe(true);
+    });
   });
 });
