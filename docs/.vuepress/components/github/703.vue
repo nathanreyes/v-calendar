041ccaa47c8941daa2737f4312f111aa2fbe7d17
Nathan Reyes <nathanreyes@me.com>
2020-11-12 07:26:27 -0600

    Update example

--- docs/.vuepress/components/github/703.vue
+++ docs/.vuepress/components/github/703.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-calendar :max-date="maxDate" :rows="2" ref="calendar" />
+    <v-date-picker :max-date="maxDate" v-model="date" ref="picker" />
     <button @click="moveToDate">Move</button>
   </div>
 </template>
@@ -10,14 +11,16 @@ export default {
   githubTitle: 'focusDate, move not working even with force',
   data() {
     return {
+      date: new Date(),
       maxDate: new Date(),
       moveDate: new Date(2021, 0, 1),
     };
   },
   methods: {
-    moveToDate() {
+    async moveToDate() {
       // this.$refs.calendar.focusDate(this.moveDate, { force: true });
       this.$refs.calendar.move(this.moveDate, { force: true });
+      await this.$refs.picker.move(this.moveDate, { force: false });
     },
   },
 };
