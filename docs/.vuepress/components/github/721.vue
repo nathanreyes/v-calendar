a1f11f6965216d87dff6f9bfa1d936b8cb5dc15a
Nathan Reyes <nathanreyes@me.com>
2020-11-26 06:49:48 -0600

    Create 721.vue

---
+++ docs/.vuepress/components/github/721.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-date-picker v-model="date" :max-date="maxDate" />
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Weird max-date prop behavour?',
+  data() {
+    return {
+      date: null,
+      maxDate: new Date(),
+    };
+  },
+};
+</script>
