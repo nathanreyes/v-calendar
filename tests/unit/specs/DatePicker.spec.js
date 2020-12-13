12bd64e16f00e641b89c06b32f83fb106f1537c5
Nathan Reyes <nathanreyes@me.com>
2020-12-13 17:41:08 -0600

    Add `max-date` test

--- tests/unit/specs/DatePicker.spec.js
+++ tests/unit/specs/DatePicker.spec.js
@@ -23,5 +23,12 @@ describe('DatePicker', () => {
       // Day of min date is not disabled
       expect(wrapper.find('.id-2000-01-05 .is-disabled').exists()).toBe(false);
     });
+    it(':max-date - prevents date after maximum date', async () => {
+      await wrapper.setProps({ maxDate: new Date(2000, 0, 25) });
+      // Day after max date is disabled
+      expect(wrapper.find('.id-2000-01-26 .is-disabled').exists()).toBe(true);
+      // Day of max date is not disabled
+      expect(wrapper.find('.id-2000-01-25 .is-disabled').exists()).toBe(false);
+    });
   });
 });
