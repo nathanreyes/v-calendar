9a8ff7af374dab709d4608e39ab8c17a3d026ffc
Nathan Reyes <nreyes@Avas-iPhone.localdomain>
2020-12-14 14:18:34 -0600

    Add tests for `is-required`

--- tests/unit/specs/DatePicker.spec.js
+++ tests/unit/specs/DatePicker.spec.js
@@ -12,6 +12,10 @@ describe('DatePicker', () => {
     });
   });
 
+  function wait(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+
   describe(':props', () => {
     it(':value - renders a date value', async () => {
       expect(wrapper.find('.id-2000-01-15').exists()).toBe(true);
@@ -24,10 +28,14 @@ describe('DatePicker', () => {
       await wrapper.find('.id-2000-01-04 .vc-day-content').trigger('click');
       // Highlight should NOT appear
       expect(wrapper.find('.id-2000-01-04 .vc-highlight').exists()).toBe(false);
+    });
+    it(':min-date - allows date on minimum date', async () => {
+      await wrapper.setProps({ minDate: new Date(2000, 0, 5) });
       // Day of min date is not disabled
       expect(wrapper.find('.id-2000-01-05 .is-disabled').exists()).toBe(false);
       // Click day of min date
       await wrapper.find('.id-2000-01-05 .vc-day-content').trigger('click');
+      await wait(1);
       // Highlight should appear
       expect(wrapper.find('.id-2000-01-05 .vc-highlight').exists()).toBe(true);
     });
@@ -39,12 +47,30 @@ describe('DatePicker', () => {
       await wrapper.find('.id-2000-01-26 .vc-day-content').trigger('click');
       // Highlight should NOT appear
       expect(wrapper.find('.id-2000-01-26 .vc-highlight').exists()).toBe(false);
+    });
+    it(':max-date - allows date on maximum date', async () => {
+      await wrapper.setProps({ maxDate: new Date(2000, 0, 25) });
       // Day of max date is not disabled
       expect(wrapper.find('.id-2000-01-25 .is-disabled').exists()).toBe(false);
       // Click day of max date
       await wrapper.find('.id-2000-01-25 .vc-day-content').trigger('click');
+      await wait(1);
       // Highlight should appear
       expect(wrapper.find('.id-2000-01-25 .vc-highlight').exists()).toBe(true);
+    });
+    it(':is-required - keeps date when set', async () => {
+      await wrapper.setProps({ isRequired: true });
+      await wrapper.find('.id-2000-01-15 .vc-day-content').trigger('click');
+      expect(wrapper.find('.id-2000-01-15 .vc-highlight').exists()).toBe(true);
+      await wrapper.find('.id-2000-01-15 .vc-day-content').trigger('click');
+      expect(wrapper.find('.id-2000-01-15 .vc-highlight').exists()).toBe(true);
+    });
+    it(':is-required - clears date when not set', async () => {
+      await wrapper.setProps({ isRequired: false });
+      await wrapper.find('.id-2000-01-15 .vc-day-content').trigger('click');
+      expect(wrapper.find('.id-2000-01-15 .vc-highlight').exists()).toBe(true);
+      await wrapper.find('.id-2000-01-15 .vc-day-content').trigger('click');
+      expect(wrapper.find('.id-2000-01-15 .vc-highlight').exists()).toBe(false);
     });
   });
 });
