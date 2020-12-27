eee77e24cbd7eba548c178f587495ac42f35b8fe
Nathan Reyes <nathanreyes@me.com>
2020-12-27 10:31:17 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   overrides: [
     {
