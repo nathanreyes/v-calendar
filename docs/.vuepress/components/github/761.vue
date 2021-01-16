72e03e8075002bb7983c8609e8bae0cbc1568dfc
Nathan Reyes <nathanreyes@me.com>
2021-01-16 15:38:05 -0600

    Add github examples

---
+++ docs/.vuepress/components/github/761.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-calendar :attributes="attributes" @dayclick="addDayAttribute" />
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Dates in attributes array not updating reactively',
+  data() {
+    return {
+      attributes: [
+        {
+          dates: new Date(2021, 0, 14),
+          highlight: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addDayAttribute(day) {
+      this.attributes.push({
+        dates: day.date,
+        highlight: true,
+      });
+    },
+  },
+};
+</script>
