20d06180398971d06cb51290d5b9d65d9360acb9
Nathan Reyes <nreyes@Nathans-MacBook-Pro.local>
2021-01-15 18:06:18 -0600

    Create 762.vue

---
+++ docs/.vuepress/components/github/762.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="example">
+    <v-calendar :min-date="minDate" :max-date="maxDate" />
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: `Can't use the popover navigation correctly when set "min-date" or "max-date"`,
+  data() {
+    return {
+      minDate: new Date(2020, 0, 12),
+      maxDate: new Date(2021, 0, 12),
+    };
+  },
+};
+</script>
