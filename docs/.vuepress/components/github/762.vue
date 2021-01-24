323759e52ca72a5ed374a5cacddc458f8bfab78f
Nathan Reyes <nathanreyes@me.com>
2021-01-24 10:40:37 -0600

    Update 762.vue

--- docs/.vuepress/components/github/762.vue
+++ docs/.vuepress/components/github/762.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="example">
+    <button @click="move">Move</button>
     <v-calendar
-      :from-page="{ month: 11, year: 2020 }"
       :min-date="minDate"
       :max-date="maxDate"
-      :can-move="canMove"
+      ref="calendar"
+      :rows="2"
     />
   </div>
 </template>
@@ -14,15 +15,18 @@ export default {
   githubTitle: `Can't use the popover navigation correctly when set "min-date" or "max-date"`,
   data() {
     return {
-      minDate: null,
-      maxDate: null,
-      // minDate: new Date(2020, 10, 10),
-      // maxDate: new Date(2020, 10, 20),
+      // minDate: null,
+      // maxDate: null,
+      minDate: new Date(2019, 10, 10), // Nov 10, 2019
+      maxDate: new Date(2021, 2, 20), // Apr 20, 2020
     };
   },
   methods: {
-    canMove(page, result) {
-      if (page.month === 2 && page.year === 2020) return false;
+    move() {
+      this.$refs.calendar.move(
+        { month: 2, year: 2020 },
+        { force: false, transition: 'slide-h' },
+      );
     },
   },
 };
