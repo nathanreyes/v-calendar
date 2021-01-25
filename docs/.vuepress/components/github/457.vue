3ca6ad61c3c73878bc2831c6c7bb4779d4111ce3
Nathan Reyes <nathanreyes@me.com>
2021-01-25 15:22:34 -0600

    Create 457.vue

---
+++ docs/.vuepress/components/github/457.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-date-picker
+    v-model="range"
+    :min-date="new Date()"
+    :disabled-dates="disabledDates"
+    is-range
+  />
+</template>
+
+<script>
+export default {
+  githubTitle: 'Disabled dates not working or ?',
+  data() {
+    return {
+      range: null,
+      disabledDates: [
+        { start: new Date(2021, 1, 9), end: new Date(2021, 1, 19) },
+      ],
+    };
+  },
+};
+</script>
