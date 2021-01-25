5d0d94d535a89434b478d2481c072f7efcf4cf9d
Nathan Reyes <nathanreyes@me.com>
2021-01-25 15:34:53 -0600

    Create 570.vue

---
+++ docs/.vuepress/components/github/570.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-date-picker
+    v-model="date"
+    :min-date="minDate"
+    :max-date="maxDate"
+    :disabled-dates="disabledDates"
+    :available-dates="availableDates"
+  />
+</template>
+
+<script>
+export default {
+  githubTitle: 'Cannot use minDate/maxDate along with available dates.',
+  data() {
+    return {
+      date: null,
+      minDate: new Date(2021, 0, 4),
+      maxDate: new Date(2021, 2, 19),
+      disabledDates: {},
+      availableDates: [
+        { start: new Date(2021, 1, 9), end: new Date(2021, 1, 19) },
+      ],
+    };
+  },
+};
+</script>
