2d698dc8876c214d337e3b8e6e28014b6039855b
Nathan Reyes <nathanreyes@me.com>
2021-01-25 13:54:17 -0600

    'Flatlogic' dummy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -102,7 +102,7 @@ module.exports = {
         {
           title: '',
           collapsable: false,
-          children: ['', 'sponsors', 'flaglogic'],
+          children: ['', 'sponsors', 'flatlogic'],
         },
         {
           title: 'Getting Started',
