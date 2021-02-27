2626a47f8674e92e9596140947aa8f644322db31
Nathan Reyes <nathanreyes@me.com>
2021-02-27 13:31:07 -0600

    Update 772.vue

--- docs/.vuepress/components/github/772.vue
+++ docs/.vuepress/components/github/772.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
+  <div class="flex flex-col">
+    <div>Time: {{ time ? time : 'Empty' }}</div>
     <v-date-picker
       v-model="time"
       mode="time"
       :model-config="modelConfig"
       :popover="{ visibility: 'click' }"
+      timezone="utc"
     >
       <template v-slot="{ inputValue, inputEvents }">
         <input
@@ -14,9 +16,45 @@
         />
       </template>
     </v-date-picker>
-    <div>
-      {{ time ? time.toISOString() : 'Empty' }}
-    </div>
+    <div>Date: {{ date ? date : 'Empty' }}</div>
+    <v-date-picker
+      v-model="date"
+      mode="dateTime"
+      :model-config="modelConfig"
+      :masks="masks"
+      :popover="{ visibility: 'visible', transition: 'none' }"
+      timezone="utc"
+    >
+      <template v-slot="{ inputValue, inputEvents }">
+        <input
+          class="px-2 py-1 rounded border-2 focus:border-blue-300 focus:outline-none"
+          :value="inputValue"
+          v-on="inputEvents"
+        />
+      </template>
+    </v-date-picker>
+    <div>Start: {{ range ? range.start : 'Empty' }}</div>
+    <div>End: {{ range ? range.end : 'Empty' }}</div>
+    <v-date-picker
+      v-model="range"
+      mode="time"
+      :model-config="modelConfig"
+      timezone="utc"
+      is-range
+    >
+      <template v-slot="{ inputValue, inputEvents }">
+        <input
+          class="px-2 py-1 rounded border-2"
+          :value="inputValue.start"
+          v-on="inputEvents.start"
+        />
+        <input
+          class="px-2 py-1 rounded border-2"
+          :value="inputValue.end"
+          v-on="inputEvents.end"
+        />
+      </template>
+    </v-date-picker>
   </div>
 </template>
 
@@ -25,11 +63,19 @@ export default {
   githubTitle: 'Time cannot be null',
   data() {
     return {
-      time: new Date(2020, 0, 1),
+      date: '2021-01-15T04:15:00.123Z',
+      range: {
+        start: '2021-01-01T00:00:00.000Z',
+        end: '2021-01-24T00:00:00.000Z',
+      },
+      time: '2021-01-15T04:15:00.000Z',
       modelConfig: {
-        fillDate: {
-          year: 1920,
-        },
+        type: 'string',
+        fillDate: new Date(1920, 0, 1),
+        // timeAdjust: '09:00',
+      },
+      masks: {
+        inputDateTime: 'MM/DD/YYYY',
       },
     };
   },
