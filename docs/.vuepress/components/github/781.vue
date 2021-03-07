33d41c71246d1a79c974dbbbf0e335c3455c4477
Nathan Reyes <nathanreyes.me@gmail.com>
2021-03-07 15:17:28 -0600

    Create 781.vue

---
+++ docs/.vuepress/components/github/781.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>Start: {{ range && range.start }}</div>
+    <div>End: {{ range && range.end }}</div>
+    <v-date-picker
+      v-model="range"
+      :min-date="new Date()"
+      mode="dateTime"
+      is24hr
+      is-range
+    >
+      <template v-slot="{ inputValue, inputEvents }">
+        <div class="flex">
+          <input
+            type="text"
+            :value="inputValue.start"
+            v-on="inputEvents.start"
+          />
+          <input type="text" :value="inputValue.end" v-on="inputEvents.end" />
+        </div>
+      </template>
+    </v-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'is24hr issue when selecting time',
+  data() {
+    return {
+      range: null,
+    };
+  },
+};
+</script>
