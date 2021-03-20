cc01ddbbda936a3c7bb4d19ab465f72251cac022
Nathan Reyes <nathanreyes.me@gmail.com>
2021-03-19 20:42:57 -0500

    Create 808.vue

---
+++ docs/.vuepress/components/github/808.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-calendar ref="calendar" />
+</template>
+
+<script>
+export default {
+  githubTitle: 'Light calendar with light popover',
+  mounted() {
+    const el = this.$el.querySelector('.vc-popover-content-wrapper');
+    if (el) {
+      el.classList.add('vc-is-dark');
+      console.log(JSON.stringify(el.classList));
+    }
+  },
+};
+</script>
