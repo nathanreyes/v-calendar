0bb14b89304ce8cbbf8282528ee0eba768e9f3ea
Nathan Reyes <nathanreyes.me@gmail.com>
2021-03-22 20:53:12 -0500

    Create 832.vue

---
+++ docs/.vuepress/components/github/832.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-date-picker v-model="range" is-range>
+    <template #default="{ inputValue, inputEvents }">
+      <input
+        class="px-2 py-1 border rounded"
+        :value="inputValue.start"
+        v-on="inputEvents.start"
+      />
+      <input
+        class="px-2 py-1 border rounded"
+        :value="inputValue.end"
+        v-on="inputEvents.end"
+      />
+    </template>
+  </v-date-picker>
+</template>
+
+<script>
+export default {
+  githubTitle: `Hitting "esc" doesn't update input`,
+  data() {
+    return {
+      range: null,
+    };
+  },
+};
+</script>
