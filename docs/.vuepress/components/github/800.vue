936aa0163d80be02796b9fd1924d08ed8b1f6122
Nathan Reyes <nathanreyes.me@gmail.com>
2021-04-05 13:11:15 -0500

    Create 800.vue

---
+++ docs/.vuepress/components/github/800.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-date-picker v-model="range" :disabled-dates="disabledDates" is-range />
+</template>
+<script>
+export default {
+  githubTitle: 'Dynamically add disabled-dates to date picker',
+  data() {
+    return {
+      range: null,
+      disabledDates: [new Date(2021, 3, 1)],
+    };
+  },
+  created() {
+    this.getDisabledDatesFromApi().then(dates => {
+      // All of these methods should work in Vue 2
+      // this.disabledDates = dates;
+      // this.disabledDates = this.disabledDates.concat(dates);
+      // this.disabledDates.push(...dates);
+
+      // This won't work in Vue 2
+      this.disabledDates[0] = dates[0];
+    });
+  },
+  methods: {
+    getDisabledDatesFromApi() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([new Date(2021, 3, 15)]);
+        }, 100);
+      });
+    },
+  },
+};
+</script>
