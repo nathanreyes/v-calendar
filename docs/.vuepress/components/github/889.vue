75fc80d3ba7a74331958c8685ae16804ce4c110f
Nathan Reyes <nathanreyes.me@gmail.com>
2021-07-25 16:34:58 -0500

    Add footer slot example

---
+++ docs/.vuepress/components/github/889.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-date-picker v-model="date" mode="datetime">
+    <template #footer> This is the footer </template>
+  </v-date-picker>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Footer slot not working',
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+};
+</script>
