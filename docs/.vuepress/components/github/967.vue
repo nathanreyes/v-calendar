78625917bd140db7baa4da75621aefbd6af35529
Nathan Reyes <nathanreyes.me@gmail.com>
2021-10-05 10:30:30 -0500

    Create 967.vue

---
+++ docs/.vuepress/components/github/967.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-start w-full">
+    <label for="visible">
+      <input id="visible" type="checkbox" v-model="visible" />
+      Visible
+    </label>
+    <v-date-picker v-if="visible" v-model="date" mode="datetime" />
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Memory issue?',
+  data() {
+    return {
+      visible: true,
+      date: new Date(),
+    };
+  },
+};
+</script>
