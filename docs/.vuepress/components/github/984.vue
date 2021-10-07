1bbe2e88edc92db9c5cce8c33eec84f03b9f4544
Nathan Reyes <nathanreyes.me@gmail.com>
2021-10-07 15:46:57 -0500

    Create 984.vue

---
+++ docs/.vuepress/components/github/984.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-date-picker
+    v-model="date"
+    mode="dateTime"
+    :minute-increment="5"
+    :popover="{ visibility: 'focus' }"
+  >
+    <template v-slot="{ inputValue, inputEvents, togglePopover }">
+      <slot v-bind="{ inputValue, inputEvents, togglePopover }">
+        <input :value="inputValue" v-on="inputEvents" />
+      </slot>
+    </template>
+  </v-date-picker>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Fix error binding v-model when using TimeSelect',
+  data() {
+    return {
+      date: new Date(2021, 8, 28, 18, 5),
+    };
+  },
+};
+</script>
