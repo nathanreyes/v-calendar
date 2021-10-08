2ae10f7d7370433614f9d8515672c45f3388264f
Nathan Reyes <nathanreyes.me@gmail.com>
2021-10-08 13:09:11 -0500

    Create 987.vue

---
+++ docs/.vuepress/components/github/987.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-calendar
+    :disabled-dates="disabledDates"
+    :available-dates="availableDates"
+    :min-date="minDate"
+  />
+</template>
+
+<script>
+export default {
+  githubTitle: 'Date enabling/disabling strategy discussion',
+  data() {
+    return {
+      disabledDates: [{ start: null, end: null }],
+      availableDates: [new Date(2021, 0, 15)],
+      minDate: new Date(2021, 0, 1),
+      minPage: { year: 2021, month: 1 },
+    };
+  },
+};
+</script>
