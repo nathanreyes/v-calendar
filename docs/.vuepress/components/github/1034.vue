2a1835953a3902d4c76c66786cd41bd8acf61151
Nathan Reyes <nathanreyes.me@gmail.com>
2022-01-17 19:28:41 -0600

    Add example

---
+++ docs/.vuepress/components/github/1034.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-date-picker v-model="range" is-range />
+</template>
+
+<script>
+import { DateInfo } from '../../../../src/lib';
+
+export default {
+  githubTitle: 'Interact with individual dates of a date-range object',
+  data() {
+    return {
+      range: null,
+    };
+  },
+  computed: {
+    datesInRange() {
+      const result = [];
+      if (!this.range) return result;
+      new DateInfo(this.range).iterateDatesInRange(this.range, ({ date }) => {
+        result.push(date);
+      });
+      return result;
+    },
+  },
+  watch: {
+    datesInRange(val) {
+      console.log('dates', val);
+    },
+  },
+};
+</script>
