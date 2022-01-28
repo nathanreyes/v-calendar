5d5d77f253c80997da50eb0aab26358146623404
Nathan Reyes <nathanreyes.me@gmail.com>
2022-01-28 08:39:03 -0600

    Next docs updates

--- docs-next/src/main.ts
+++ docs-next/src/main.ts
@@ -1,11 +1,24 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { SetupCalendar, Calendar, DatePicker } from '../../src/index';
 import './assets/styles/tailwind.css';
 
+const defaults = {
+  // componentPrefix: '',
+  titlePosition: 'left',
+};
+
+// // declare const window: any;
+// // window.__vcalendar__ = defaults;
+
+// Method 1
+// import VCalendar from '../../src/index';
+// createApp(App).use(VCalendar, defaults).use(router).mount('#app');
+
+// // Method 2
+import { SetupCalendar, Calendar, DatePicker } from '../../src/index';
 createApp(App)
-  .use(SetupCalendar, {})
+  .use(SetupCalendar, defaults)
   .component('Calendar', Calendar)
   .component('DatePicker', DatePicker)
   .use(router)
