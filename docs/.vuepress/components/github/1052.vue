9e973bf750b6caba2c6c4acb01cbc878795fabda
Nathan Reyes <nathanreyes.me@gmail.com>
2022-01-30 15:57:43 -0600

    Create 1052.vue

---
+++ docs/.vuepress/components/github/1052.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-date-picker v-model="date" :model-config="modelConfig">
+      <template v-slot="{ inputValue, inputEvents }">
+        <input :value="inputValue" v-on="inputEvents" />
+      </template>
+    </v-date-picker>
+    {{ date }}
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Interact with individual dates of a date-range object',
+  data() {
+    return {
+      date: null,
+      modelConfig: {
+        type: 'string',
+        mask: 'iso',
+      },
+    };
+  },
+};
+</script>
