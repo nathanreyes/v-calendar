efd0b08b28d23e853507378aa712e598967566f8
Nathan Reyes <nathanreyes.me@gmail.com>
2022-02-05 21:03:25 -0600

    Create 000.vue

---
+++ docs/.vuepress/components/github/000.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <v-date-picker
+        mode="dateTime"
+        v-model="date"
+        :model-config="modelConfig"
+        timezone="UTC"
+      >
+        <template #default="{ inputValue, inputEvents }">
+          <input :value="inputValue" v-on="inputEvents" />
+        </template>
+      </v-date-picker>
+    </div>
+    <div>{{ date }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  githubTitle: 'Open Test',
+  data() {
+    return {
+      date: '2022-02-09T00:42:00.000Z',
+      modelConfig: {
+        type: 'string',
+        mask: 'iso',
+        // fillDate: new Date(2021, 0, 1),
+        validHours: [0, 3, 4, 5, 8, 16, 20],
+      },
+    };
+  },
+};
+</script>
