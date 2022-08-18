cc0237464fc3ae8a4ea2511512e688f1c59eebd8
Nathan Reyes <nathanreyes.me@gmail.com>
2022-08-18 15:36:42 -0500

    Export `CalendarGrid` component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Calendar } from './Calendar/Calendar.vue';
+export { default as CalendarGrid } from './CalendarGrid/CalendarGrid.vue';
 export { default as DatePicker } from './DatePicker/DatePicker.vue';
 export { default as Popover } from './Popover/Popover.vue';
 export { default as PopoverRow } from './PopoverRow/PopoverRow.vue';
