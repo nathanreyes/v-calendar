856b8064f7a70e444f87e2227949944a9f0d4398
Nathan Reyes <nathanreyes.me@gmail.com>
2022-10-03 10:42:30 -0500

    Extract timePicker to use function

--- src/components/TimePicker/TimePicker.vue
+++ src/components/TimePicker/TimePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="vc-time-picker"
-    :class="[{ 'vc-invalid': !(parts as DateParts).isValid, 'vc-bordered': showBorder }]"
+    :class="[{ 'vc-invalid': !isValid, 'vc-bordered': showBorder }]"
   >
     <slot name="time-header">
       <div v-if="showHeader" class="vc-time-header">
@@ -36,11 +36,11 @@
         <span class="vc-time-colon">:</span>
         <TimeSelect v-model.number="minutes" :options="options.minutes" />
       </template>
-      <template v-if="timeAccuracy > 2 && hasValue">
+      <template v-if="timeAccuracy > 2">
         <span class="vc-time-colon">:</span>
         <TimeSelect v-model.number="seconds" :options="options.seconds" />
       </template>
-      <template v-if="timeAccuracy > 3 && hasValue">
+      <template v-if="timeAccuracy > 3">
         <span class="vc-time-decimal">.</span>
         <TimeSelect
           v-model.number="milliseconds"
@@ -53,180 +53,28 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
 import TimeSelect from '../TimeSelect/TimeSelect.vue';
-import { arrayHasItems } from '../../utils/helpers';
-import { DateParts, DatePatch, getDatePartsOptions } from '../../utils/dates';
-import { useBase } from '../../use/base';
-import { ValueTarget, useDatePicker } from '../../use/datePicker';
-
-const _amOptions = [
-  { value: 0, label: '12' },
-  { value: 1, label: '1' },
-  { value: 2, label: '2' },
-  { value: 3, label: '3' },
-  { value: 4, label: '4' },
-  { value: 5, label: '5' },
-  { value: 6, label: '6' },
-  { value: 7, label: '7' },
-  { value: 8, label: '8' },
-  { value: 9, label: '9' },
-  { value: 10, label: '10' },
-  { value: 11, label: '11' },
-];
-const _pmOptions = [
-  { value: 12, label: '12' },
-  { value: 13, label: '1' },
-  { value: 14, label: '2' },
-  { value: 15, label: '3' },
-  { value: 16, label: '4' },
-  { value: 17, label: '5' },
-  { value: 18, label: '6' },
-  { value: 19, label: '7' },
-  { value: 20, label: '8' },
-  { value: 21, label: '9' },
-  { value: 22, label: '10' },
-  { value: 23, label: '11' },
-];
+import { createTimePicker } from '../../use/timePicker';
 
 const props = defineProps<{
   position: number;
 }>();
 
-const { locale } = useBase();
 const {
-  isRange,
-  isTime,
-  dateParts,
-  modelConfig,
+  locale,
+  isValid,
+  date,
+  hours,
+  minutes,
+  seconds,
+  milliseconds,
+  options,
+  hourOptions,
+  isAM,
+  isAMOptions,
   is24hr,
-  hideTimeHeader,
+  showHeader,
+  showBorder,
   timeAccuracy,
-  updateValue: updateDpValue,
-} = useDatePicker();
-
-interface TimeParts {
-  hours: number;
-  minutes: number;
-  seconds: number;
-  milliseconds: number;
-}
-
-function updateParts(newParts: Partial<TimeParts>) {
-  newParts = Object.assign(parts.value, newParts);
-  let newValue = null;
-  if (isRange.value) {
-    const start = isStart.value ? newParts : dateParts.value[0];
-    const end = isStart.value ? dateParts.value[1] : newParts;
-    newValue = { start, end };
-  } else {
-    newValue = newParts;
-  }
-  updateDpValue(newValue, {
-    patch: DatePatch.Time,
-    rangePriority: isStart.value ? ValueTarget.Start : ValueTarget.End,
-    moveToValue: isStart.value ? ValueTarget.Start : ValueTarget.End,
-  });
-}
-
-const isStart = computed(() => props.position === 0);
-const rules = computed(() => modelConfig.value[props.position].rules);
-const parts = computed(() => dateParts.value[props.position]);
-const showBorder = computed(() => !isTime.value);
-const hasValue = computed(
-  () => parts.value && (parts.value as DateParts).hours != null,
-);
-const showHeader = computed(() => {
-  return !hideTimeHeader.value && hasValue.value;
-});
-
-const date = computed(() => {
-  let date = locale.value.normalizeDate(parts.value);
-  if ((parts.value as DateParts).hours === 24) {
-    date = new Date(date.getTime() - 1);
-  }
-  return date;
-});
-
-const hours = computed<number>({
-  get() {
-    return (parts.value as DateParts).hours;
-  },
-  set(val) {
-    updateParts({ hours: val });
-  },
-});
-
-const minutes = computed<number>({
-  get() {
-    return (parts.value as DateParts).minutes;
-  },
-  set(val) {
-    updateParts({ minutes: val });
-  },
-});
-
-const seconds = computed<number>({
-  get() {
-    return (parts.value as DateParts).seconds;
-  },
-  set(val) {
-    updateParts({ seconds: val });
-  },
-});
-
-const milliseconds = computed<number>({
-  get() {
-    return (parts.value as DateParts).milliseconds;
-  },
-  set(val) {
-    updateParts({ milliseconds: val });
-  },
-});
-
-const isAM = computed({
-  get() {
-    return (parts.value as DateParts).hours < 12;
-  },
-  set(value) {
-    let hValue = hours.value;
-    if (value && hValue >= 12) {
-      hValue -= 12;
-    } else if (!value && hValue < 12) {
-      hValue += 12;
-    }
-    updateParts({ hours: hValue });
-  },
-});
-
-const options = computed(() =>
-  getDatePartsOptions(parts.value as DateParts, rules.value),
-);
-
-const amHourOptions = computed(() => {
-  return _amOptions.filter(opt =>
-    options.value.hours.some(ho => ho.value === opt.value),
-  );
-});
-
-const pmHourOptions = computed(() => {
-  return _pmOptions.filter(opt =>
-    options.value.hours.some(ho => ho.value === opt.value),
-  );
-});
-
-const hourOptions = computed(() => {
-  if (is24hr.value) return options.value.hours;
-  if (isAM.value) return amHourOptions.value;
-  return pmHourOptions.value;
-});
-
-const isAMOptions = computed(() => {
-  const result = [];
-  if (arrayHasItems(amHourOptions.value))
-    result.push({ value: true, label: 'AM' });
-  if (arrayHasItems(pmHourOptions.value))
-    result.push({ value: false, label: 'PM' });
-  return result;
-});
+} = createTimePicker(props);
 </script>
--- src/use/timePicker.ts
+++ src/use/timePicker.ts
@@ -1,8 +1,8 @@
 import { computed } from 'vue';
 import { arrayHasItems } from '../utils/helpers';
 import { DateParts, DatePatch, getDatePartsOptions } from '../utils/dates';
-import { useBase } from './base';
-import { RangePriority, useDatePicker } from './datePicker';
+import { useBase } from '../use/base';
+import { ValueTarget, useDatePicker } from '../use/datePicker';
 
 const _amOptions = [
   { value: 0, label: '12' },
@@ -33,4 +33,164 @@ const _pmOptions = [
   { value: 23, label: '11' },
 ];
 
-export default function useTimePicker() {}
+export interface TimePickerProps {
+  position: number;
+}
+
+export function createTimePicker(props: TimePickerProps) {
+  const baseCtx = useBase();
+  const { locale } = baseCtx;
+  const {
+    isRange,
+    isTime,
+    dateParts,
+    modelConfig,
+    is24hr,
+    hideTimeHeader,
+    timeAccuracy,
+    updateValue: updateDpValue,
+  } = useDatePicker();
+
+  interface TimeParts {
+    hours: number;
+    minutes: number;
+    seconds: number;
+    milliseconds: number;
+  }
+
+  function updateParts(newParts: Partial<TimeParts>) {
+    newParts = Object.assign(parts.value, newParts);
+    let newValue = null;
+    if (isRange.value) {
+      const start = isStart.value ? newParts : dateParts.value[0];
+      const end = isStart.value ? dateParts.value[1] : newParts;
+      newValue = { start, end };
+    } else {
+      newValue = newParts;
+    }
+    updateDpValue(newValue, {
+      patch: DatePatch.Time,
+      targetPriority: isStart.value ? ValueTarget.Start : ValueTarget.End,
+      moveToValue: true,
+    });
+  }
+
+  const isStart = computed(() => props.position === 0);
+  const rules = computed(() => modelConfig.value[props.position].rules);
+  const parts = computed(() => dateParts.value[props.position]);
+  const isValid = computed(() => !!parts.value.isValid);
+  const showBorder = computed(() => !isTime.value);
+  const showHeader = computed(() => {
+    return !hideTimeHeader.value && isValid.value;
+  });
+
+  const date = computed(() => {
+    let date = locale.value.normalizeDate(parts.value);
+    if ((parts.value as DateParts).hours === 24) {
+      date = new Date(date.getTime() - 1);
+    }
+    return date;
+  });
+
+  const hours = computed<number>({
+    get() {
+      return (parts.value as DateParts).hours;
+    },
+    set(val) {
+      updateParts({ hours: val });
+    },
+  });
+
+  const minutes = computed<number>({
+    get() {
+      return (parts.value as DateParts).minutes;
+    },
+    set(val) {
+      updateParts({ minutes: val });
+    },
+  });
+
+  const seconds = computed<number>({
+    get() {
+      return (parts.value as DateParts).seconds;
+    },
+    set(val) {
+      updateParts({ seconds: val });
+    },
+  });
+
+  const milliseconds = computed<number>({
+    get() {
+      return (parts.value as DateParts).milliseconds;
+    },
+    set(val) {
+      updateParts({ milliseconds: val });
+    },
+  });
+
+  const isAM = computed({
+    get() {
+      return (parts.value as DateParts).hours < 12;
+    },
+    set(value) {
+      value = String(value).toLowerCase() == 'true';
+      let hValue = hours.value;
+      if (value && hValue >= 12) {
+        hValue -= 12;
+      } else if (!value && hValue < 12) {
+        hValue += 12;
+      }
+      updateParts({ hours: hValue });
+    },
+  });
+
+  const options = computed(() =>
+    getDatePartsOptions(parts.value as DateParts, rules.value),
+  );
+
+  const amHourOptions = computed(() => {
+    return _amOptions.filter(opt =>
+      options.value.hours.some(ho => ho.value === opt.value),
+    );
+  });
+
+  const pmHourOptions = computed(() => {
+    return _pmOptions.filter(opt =>
+      options.value.hours.some(ho => ho.value === opt.value),
+    );
+  });
+
+  const hourOptions = computed(() => {
+    if (is24hr.value) return options.value.hours;
+    if (isAM.value) return amHourOptions.value;
+    return pmHourOptions.value;
+  });
+
+  const isAMOptions = computed(() => {
+    const result = [];
+    if (arrayHasItems(amHourOptions.value))
+      result.push({ value: true, label: 'AM' });
+    if (arrayHasItems(pmHourOptions.value))
+      result.push({ value: false, label: 'PM' });
+    return result;
+  });
+
+  return {
+    ...baseCtx,
+    showBorder,
+    showHeader,
+    timeAccuracy,
+    parts,
+    isValid,
+    date,
+    hours,
+    minutes,
+    seconds,
+    milliseconds,
+    options,
+    hourOptions,
+    isAM,
+    isAMOptions,
+    is24hr,
+  };
+}
