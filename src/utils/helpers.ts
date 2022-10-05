1864f1dfd62e05bc0c5514251e5b9a986702a786
Nathan Reyes <nathanreyes.me@gmail.com>
2022-10-05 09:26:59 -0500

    Pick only existing keys

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -185,7 +185,7 @@ export const omit = <T extends object, K extends [...(keyof T)[]]>(
 export const pick = <T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> => {
   const ret: any = {};
   keys.forEach(key => {
-    ret[key] = obj[key];
+    if (key in obj) ret[key] = obj[key];
   });
   return ret;
 };
