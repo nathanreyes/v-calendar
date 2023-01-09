ceacab5c26a17658494bdc5a1580efd080543f02
Nathan Reyes <nathanreyes.me@gmail.com>
2023-01-09 16:07:57 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+/// <reference types="vitest" />
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
