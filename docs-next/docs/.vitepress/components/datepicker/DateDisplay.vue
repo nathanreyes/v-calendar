80145c0a3e912d68703afba802810dc8e6666024
Nathan Reyes <nathanreyes.me@gmail.com>
2023-01-12 13:34:54 -0600

    Update DateDisplay.vue

--- docs-next/docs/.vitepress/components/datepicker/DateDisplay.vue
+++ docs-next/docs/.vitepress/components/datepicker/DateDisplay.vue
@@ -3,24 +3,31 @@
     <div class="space-y-2 mb-4">
       <!--Date Display (ISO)-->
       <div class="flex space-x-6" v-if="!isRange">
-        <BaseField label="Date Type:">{{ dateType(value) }}</BaseField>
-        <BaseField label="Date Value:">{{ dateLabel(value) }}</BaseField>
+        <BaseField label="Value:"
+          >{{ dateRaw(value) }}
+          <span v-if="dateType(value)"
+            >({{ capitalize(dateType(value)) }})</span
+          ></BaseField
+        >
+        <BaseField label="ISO:">{{ dateISO(value) }}</BaseField>
       </div>
       <!--Date Range Display (ISO)-->
       <template v-else>
         <BaseField v-if="value == null" label="Date Value"> null </BaseField>
         <template v-else>
           <div class="flex space-x-4">
-            <BaseField label="Start Type:">{{
-              dateType(value.start)
-            }}</BaseField>
-            <BaseField label="Start Value:">{{
-              dateLabel(value.start)
-            }}</BaseField>
-          </div>
-          <div class="flex space-x-4">
-            <BaseField label="End Type:">{{ dateType(value.end) }}</BaseField>
-            <BaseField label="End Value:">{{ dateLabel(value.end) }}</BaseField>
+            <BaseField label="Start:"
+              >{{ dateRaw(value.start) }}
+              <span v-if="dateType(value.start)"
+                >({{ dateType(value.start) }})</span
+              ></BaseField
+            >
+            <BaseField label="End:"
+              >{{ dateRaw(value.end) }}
+              <span v-if="dateType(value.end)"
+                >({{ dateType(value.end) }})</span
+              ></BaseField
+            >
           </div>
         </template>
       </template>
@@ -36,7 +43,9 @@
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue';
+import { computed } from 'vue';
+import { capitalize } from 'lodash';
+import Locale from '@plugin/utils/locale';
 
 export default {
   inheritAttrs: false,
@@ -49,20 +58,28 @@ export default {
       return attrs.modelModifiers.range === true;
     });
     function dateType(val) {
-      if (val == null) return 'null';
+      if (val == null) return '';
       if (val.toISOString) return 'date';
       return typeof val;
     }
-    function dateLabel(val) {
+    function dateRaw(val) {
       if (val == null) return 'null';
-      if (val.toISOString) return val.toISOString();
-      return val;
+      if (val.toLocaleString) return val.toLocaleString();
+      return val.toString();
+    }
+    function dateISO(val) {
+      if (val == null) return 'null';
+      const locale = new Locale();
+      const date = locale.toDate(val);
+      return date.toISOString();
     }
     return {
       value,
       isRange,
       dateType,
-      dateLabel,
+      dateRaw,
+      dateISO,
+      capitalize,
     };
   },
 };
