196a282105521ae541eb67b4c6791fbb376f5a49
Nathan Reyes <nathanreyes.me@gmail.com>
2023-01-12 13:32:57 -0600

    Update TimezonesPickerTime.vue

--- docs-next/docs/.vitepress/components/i18n/TimezonesPickerTime.vue
+++ docs-next/docs/.vitepress/components/i18n/TimezonesPickerTime.vue
@@ -13,7 +13,7 @@ import { ref } from 'vue';
 
 const timezone = ref('UTC');
 const range = {
-  start: Date.UTC(2020, 0, 6),
-  end: Date.UTC(2020, 0, 10),
+  start: new Date(Date.UTC(2020, 0, 6)),
+  end: new Date(Date.UTC(2020, 0, 10)),
 };
 </script>
