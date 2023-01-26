b430a3a98f970ebe70c41214aa72188293830549
Nathan Reyes <nathanreyes.me@gmail.com>
2023-01-26 14:53:26 -0600

    Add min-date, max-date rules

--- tests/unit/specs/DatePicker.spec.ts
+++ tests/unit/specs/DatePicker.spec.ts
@@ -161,71 +161,57 @@ describe('DatePicker', () => {
       });
     }
 
-    // it(':min-date - prevents date before minimum date', async () => {
-    //   const dp = mount(DatePicker, {
-    //     props: {
-    //       modelValue: new Date(2000, 0, 15),
-    //       minDate: new Date(2000, 0, 5),
-    //     },
-    //   });
-    //   await dp.vm.$nextTick();
-    //   // Day before min date is disabled
-    //   expect(dp.find('.id-2000-01-04 .is-disabled').exists()).toBe(true);
-    //   // Click day before min date
-    //   await dp.find('.id-2000-01-04 .vc-day-content').trigger('click');
-    //   await dp.vm.$nextTick();
-    //   // Highlight should NOT appear
-    //   expect(dp.find('.id-2000-01-04 .vc-highlight').exists()).toBe(false);
-    // });
+    it(':min-date - prevents date before minimum date', async () => {
+      const dp = await mountDp({
+        modelValue: new Date(2000, 0, 15),
+        minDate: new Date(2000, 0, 5),
+      });
+      // Day before min date is disabled
+      expect(dp.find('.id-2000-01-04 .vc-disabled').exists()).toEqual(true);
+      // Click day before min date
+      await dp.find('.id-2000-01-04 .vc-day-content').trigger('click');
+      // Highlight should NOT appear
+      expect(dp.find('.id-2000-01-04 .vc-highlight').exists()).toEqual(false);
+    });
 
-    // it(':min-date - allows date on minimum date', async () => {
-    //   const dp = mount(DatePicker, {
-    //     props: {
-    //       modelValue: new Date(2000, 0, 15),
-    //       minDate: new Date(2000, 0, 5),
-    //     },
-    //   });
-    //   await dp.vm.$nextTick();
-    //   // Day of min date is not disabled
-    //   expect(dp.find('.id-2000-01-05 .is-disabled').exists()).toBe(false);
-    //   console.log(dp.find('.id-2000-01-05 .vc-day-content').element);
-    //   // Click day of min date
-    //   await dp.find('.id-2000-01-05 .vc-day-content').trigger('click');
-    //   // Highlight should appear
-    //   expect(dp.find('.id-2000-01-05 .vc-highlight').exists()).toBe(true);
-    // });
+    it(':min-date - allows date on minimum date', async () => {
+      const dp = await mountDp({
+        modelValue: new Date(2000, 0, 15),
+        minDate: new Date(2000, 0, 5),
+      });
+      // Day of min date is not disabled
+      expect(dp.find('.id-2000-01-05 .vc-disabled').exists()).toEqual(false);
+      // Click day of min date
+      await dp.find('.id-2000-01-05 .vc-day-content').trigger('click');
+      // Highlight should appear
+      expect(dp.find('.id-2000-01-05 .vc-highlight').exists()).toEqual(true);
+    });
 
-    // it(':max-date - prevents date after maximum date', async () => {
-    //   const dp = mount(DatePicker, {
-    //     props: {
-    //       modelValue: new Date(2000, 0, 15),
-    //       maxDate: new Date(2000, 0, 25),
-    //     },
-    //   });
-    //   await dp.vm.$nextTick();
-    //   // Day after max date is disabled
-    //   expect(dp.find('.id-2000-01-26 .is-disabled').exists()).toBe(true);
-    //   // Click day after max date
-    //   await dp.find('.id-2000-01-26 .vc-day-content').trigger('click');
-    //   // Highlight should NOT appear
-    //   expect(dp.find('.id-2000-01-26 .vc-highlight').exists()).toBe(false);
-    // });
+    it(':max-date - prevents date after maximum date', async () => {
+      const dp = await mountDp({
+        modelValue: new Date(2000, 0, 15),
+        maxDate: new Date(2000, 0, 25),
+      });
+      // Day after max date is disabled
+      expect(dp.find('.id-2000-01-26 .vc-disabled').exists()).toBe(true);
+      // Click day after max date
+      await dp.find('.id-2000-01-26 .vc-day-content').trigger('click');
+      // Highlight should NOT appear
+      expect(dp.find('.id-2000-01-26 .vc-highlight').exists()).toBe(false);
+    });
 
-    // it(':max-date - allows date on maximum date', async () => {
-    //   const dp = mount(DatePicker, {
-    //     props: {
-    //       modelValue: new Date(2000, 0, 15),
-    //       maxDate: new Date(2000, 0, 25),
-    //     },
-    //   });
-    //   await dp.vm.$nextTick();
-    //   // Day of max date is not disabled
-    //   expect(dp.find('.id-2000-01-25 .is-disabled').exists()).toBe(false);
-    //   // Click day of max date
-    //   await dp.find('.id-2000-01-25 .vc-day-content').trigger('click');
-    //   // Highlight should appear
-    //   expect(dp.find('.id-2000-01-25 .vc-highlight').exists()).toBe(true);
-    // });
+    it(':max-date - allows date on maximum date', async () => {
+      const dp = await mountDp({
+        modelValue: new Date(2000, 0, 15),
+        maxDate: new Date(2000, 0, 25),
+      });
+      // Day of max date is not disabled
+      expect(dp.find('.id-2000-01-25 .vc-disabled').exists()).toBe(false);
+      // Click day of max date
+      await dp.find('.id-2000-01-25 .vc-day-content').trigger('click');
+      // Highlight should appear
+      expect(dp.find('.id-2000-01-25 .vc-highlight').exists()).toBe(true);
+    });
 
     // it(':model-config.fillDate - fills missing date parts for date input', async () => {
     //   const dp = mountWithInputs({
