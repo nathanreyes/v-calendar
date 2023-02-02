db75bc04ea77e266a6bfe3202d2f151c27f54f1f
Nathan Reyes <nathanreyes.me@gmail.com>
2023-02-02 16:06:21 -0600

    Update vite.config.ts

--- docs/docs/vite.config.ts
+++ docs/docs/vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [nodeResolve({ exportConditions: ['node'] })],
+  plugins: [
+    nodeResolve({
+      exportConditions: ['node'],
+      modulePaths: [path.resolve(__dirname, '../')],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
