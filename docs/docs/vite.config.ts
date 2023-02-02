e2afcd244fbe428b223cb43ef26850d282edbaa7
Nathan Reyes <nathanreyes.me@gmail.com>
2023-02-02 17:24:30 -0600

    Update vite.config.ts

--- docs/docs/vite.config.ts
+++ docs/docs/vite.config.ts
@@ -1,23 +1,10 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
-console.log(path.resolve(__dirname, '../node_modules'));
-console.log(path.resolve(__dirname, '../../node_modules'));
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    nodeResolve({
-      exportConditions: ['node'],
-      modulePaths: [
-        path.resolve(__dirname, '../node_modules'),
-        path.resolve(__dirname, '../../node_modules'),
-      ],
-    }),
-  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
