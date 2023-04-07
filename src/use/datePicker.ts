f7517b1fe74a59e652519019048d27657cb1d309
Ioan Cristea <ioan.cristea@krioft.com>
2023-04-07 15:51:55 +0300

    datePicker.ts update for dateTime mode
    If we have "dateTime" mode and select a date, the popover is closed, and need to reopen the popover to select the time.
    When we use "dateTime" mode we want to select Date + Time.
    Please merge this as hotfix.

--- src/use/datePicker.ts
+++ src/use/datePicker.ts
@@ -643,7 +643,7 @@ export function createDatePicker(
     const opts: Partial<UpdateOptions> = {
       patch: 'date',
       formatInput: true,
-      hidePopover: true,
+      hidePopover: !isDateTimeMode.value,
     };
     if (isRange.value) {
       const dragging = !isDragging.value;
