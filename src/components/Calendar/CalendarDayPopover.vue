3596079b5b4f932809650246396647f8c3d6e85e
Nathan Reyes <nathanreyes.me@gmail.com>
2023-09-09 22:45:46 -0500

    Move PopoverRow to Popover folder

--- src/components/Calendar/CalendarDayPopover.vue
+++ src/components/Calendar/CalendarDayPopover.vue
@@ -29,7 +29,7 @@
 import { useCalendar } from '../../use/calendar';
 import { CalendarDay } from '../../utils/page';
 import Popover from '../Popover/Popover.vue';
-import PopoverRow from '../PopoverRow/PopoverRow.vue';
+import PopoverRow from '../Popover/PopoverRow.vue';
 import CalendarSlot from './CalendarSlot.vue';
 
 const { dayPopoverId, displayMode, color, masks, locale } = useCalendar();
