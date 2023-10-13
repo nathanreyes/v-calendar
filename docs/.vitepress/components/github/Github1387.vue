e5bd4458c607ecbe950ac830a34ef8f1a712ade8
Nathan Reyes <nathanreyes.me@gmail.com>
2023-10-13 06:27:59 -0500

    Create Github1387.vue

---
+++ docs/.vitepress/components/github/Github1387.vue
@@ -0,0 +1,17 @@
+<template>
+  <VDatePicker v-model="date" locale="de">
+    <template #default="{ inputValue, inputEvents }">
+      <input
+        :value="inputValue"
+        v-on="inputEvents"
+        class="px-2 py-1 bg-white text-gray-800 rounded"
+      />
+    </template>
+  </VDatePicker>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const date = ref(new Date());
+</script>
